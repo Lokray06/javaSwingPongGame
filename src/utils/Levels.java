1d411b7ed0df298f40a8d650a8c8ca5e5a7a2d1d
Lokray06 <jpgp14s06@gmail.com>
2025-01-17 11:49:00 +0100

    OK 3rd commit, almost finished

--- src/utils/Levels.java
+++ src/utils/Levels.java
@@ -12,6 +12,6 @@ public class Levels
     {
         levels.put("Easy", new GameModeDTO(30, 20, 600, new boolean[]{true, true, true, false}, 4, 1, 120, 5, 1, 3, 6, 50));
         levels.put("Medium", new GameModeDTO(50, 20, 300, new boolean[]{true, false, true, false}, 5, 2, 15, 5, 1, 3, 6, 30));
-        levels.put("Cursed", new GameModeDTO(500, 40, 700, new boolean[]{true, false, true, false}, 6, 2, 20, 10, 1, 3, 6, 10));
+        levels.put("Cursed", new GameModeDTO(500, 40, 700, new boolean[]{true, false, true, false}, 6, 2, 20, 10, 1, 3, 7, 10));
     }
 }
